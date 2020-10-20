d0884d7b3ced0d01e24b210284b9370432964274
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-10-20 19:29:14 +0200

    Update

--- 5.trees/indentical_tree.py
+++ 5.trees/indentical_tree.py
@@ -9,6 +9,11 @@ class Solution:
     def __init__(self):
         super().__init__()
 
+    # proper way
     def is_same_tree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p and q:
+            return p.val == q.val and self.is_same_tree(p.left, q.left) and self.is_same_tree(p.right, q.right)
+        return p is q
 
-        return True
+
+# why p is q. It is just to return True if p==None and q==None else False.
